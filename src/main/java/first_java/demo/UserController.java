c8c361faaa4e8db1a570f61899e723161f1ad931
waddu <yhy6923@naver.com>
2025-05-21 20:31:52 +0900

    CRUD 기능 구현

--- src/main/java/first_java/demo/UserController.java
+++ src/main/java/first_java/demo/UserController.java
@@ -23,4 +23,19 @@ public class UserController{
     public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user)
+        { return userRepository.findById(id).map(
+            existing-> {
+                existing.setUsername(user.getUsername());
+                existing.setEmail(user.getEmail());
+                return userRepository.save(existing);
+            }).orElseThrow(()-> new RuntimeException("User not found!"));
+        }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
 }
--- src/main/java/first_java/demo/UserRepository.java
+++ src/main/java/first_java/demo/UserRepository.java
@@ -3,4 +3,7 @@ package first_java.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+    Long id(Long id);
+
+    Long Id(Long id);
 }
